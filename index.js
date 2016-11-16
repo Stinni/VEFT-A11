6149d11c815d5d7d9472199a7551e61ea7c616f2
Stinni <kristinnf13@ru.is>
2016-11-16 12:38:38 +0000

    ready for handin

--- index.js
+++ index.js
@@ -2,20 +2,18 @@
 // Student: Kristinn Heiðar Freysteinsson
 // Email: kristinnf13@ru.is
 
-const express    = require("express");
-const bodyParser = require("body-parser");
-
-const app = express();
-// only accept json body
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+const port = 4000;
+const express = require("express");
+const app     = express();
 
 /*
 /hello - GET
 Returns the greeting 'Hello World'
 */
-app.get("/hello", (req, res) => {
-	res.code(200).send("Hello World");
+app.get("/api/hello", (req, res) => {
+	res.status(200).send("Hello World");
 });
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || port, () => {
+	console.log("Server starting on port " + port);
+});
